2613ebd63331a78deecbf043aa364bc34ff9a1e9
deepti1311 <deeptijakhotra@gmail.com>
2022-09-19 15:49:38 +0530

    "initial readme file"

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
 
-	fmt.Println("hello Go...")
+	//fmt.Println("hello Go...")
+	r := gim.Defalt()
 
+	r.GET("/hello", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello REST API")
+	})
 }
